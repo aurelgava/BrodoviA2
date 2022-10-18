8ed05c477068bc994b702d60fe7c86d7780d8bba
Darko Lunc <10219darkolunc@etsntesla.edu.rs>
2022-10-18 11:08:40 +0200

    Konstruktori za klase Brod, Mornar, Gusarksi broj i Kraljevski brod

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,8 @@ import more.*;
                 g1.dodajMorn(new Mornar("E", 60));
                 k1.dodajMorn(m4);
                 System.out.println("Broj mornara na gusarskom brodu: " + g1.dohvBr());
-                /*System.out.println("Kapetan gusarskog broda: " + g1.dohvatiKapetana());
+                System.out.println("Kapetan gusarskog broda: " + g1.dohvatiKapetana());
+
 
                 try {
                     System.out.println("Mornar kraljevskog broda na poziciji 1: " + k1.dohvMorn(1));
